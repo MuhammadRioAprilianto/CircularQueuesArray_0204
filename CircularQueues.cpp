e01943f8562f12a43f549c5c3da858d316c7c566
MuhammadRioAprilianto <rma994636@gmail.com>
2025-06-12 10:11:47 +0700

    membuat penanda eleman pertama dan terakhir

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -4,4 +4,5 @@ using namespace std;
 class Queues{
     private:
     static const int max = 5; //memubuat array dengan panjang 5 index
+    int FRONT, REAR; // membuat penanda eleman pertama dan terakhir
 };
