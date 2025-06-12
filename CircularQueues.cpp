ee0b1484881b7517c0dc582f42417d50b6021649
MuhammadRioAprilianto <rma994636@gmail.com>
2025-06-12 10:48:40 +0700

    memubuat objek q

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -82,5 +82,39 @@ class Queues{
         }
 
         cout << "\nElements in the queue are...\n";
+
+        //jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+        if (FRONT_position <= REAR_position)
+        {
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queues_array[FRONT_position] << "   ";
+                FRONT_position;
+            }
+            cout << endl;
+        }
+        else
+        {
+            //jika FRONT_position > REAR_position, iterasi dari FRONT hingga array
+            while (FRONT_position <= max -1)
+            {
+                cout << queues_array[FRONT_position] << "   ";
+                FRONT_position++;
+            }
+            FRONT_position = 0;
+
+            // iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queues_array[FRONT_position] << "   ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
     }
 };
+
+int main()
+{
+    Queues q; 
+}
